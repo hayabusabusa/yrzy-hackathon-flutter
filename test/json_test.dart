c8d02cf29296815a81a62d657f2ac5aa89099203
hayabusabusa <hayabusabusaSY@gmail.com>
2021-04-12 23:23:34 +0900

    test: add json format check test

---
+++ test/json_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:yrzy_hackathon/utils/utils.dart';
+
+void main() {
+  // NOTE: Assets からの読み込みのため.
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('JSON のテスト', () {
+    test('フォーマットが正しいことをサンプルで検知できること', () {
+      Json.load(JsonFile.Sample).then((jsonString) {
+        expect(Json.checkFormat(jsonString), true);
+      });
+    });
+  });
+}
