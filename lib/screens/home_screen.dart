463710354c62951795a6f3340c34bbede8672bb3
MYCOMPUTER\aokaz <sirokuma0425@gmail.com>
2021-04-17 14:09:30 +0900

    ホーム画面作りました（笑）

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,8 +4,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('home_screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Column(
+              children: [
+                Text(
+                  '~Yorozuya presents~',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                Text(
+                  'クイズアプリ',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                SizedBox(height: 12,),
+                Text(
+                  '貴様にこの問題が解けるか？\n簡単な問題ばかりなので\n100点目指して頑張りましょう！',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(child: SizedBox()),
+            ElevatedButton(
+              onPressed: () {}, 
+              child: Text('クイズを始める')
+            ),
+          ],
+        ),
       ),
     );
   }
