61ede0ec0b130e56383a59973a35d8984b6a61a2
hayabusabusa <hayabusabusaSY@gmail.com>
2021-04-18 23:27:58 +0900

    ui: add fade route

---
+++ lib/utils/fade_route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FadeRoute extends PageRouteBuilder {
+  final Widget page;
+
+  FadeRoute(
+    this.page,
+  ): super(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, _, child) => FadeTransition(opacity: animation, child: child,),
+    );
+}
