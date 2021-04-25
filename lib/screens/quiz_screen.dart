190e39cbc41410f28059d6c1fd3d48fde1e3ee86
Arata Totsuka <bump.art71@gmail.com>
2021-04-25 21:37:03 +0900

    Changed list shuffle and fixed indentations

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -15,12 +15,9 @@ class _QuizScreenState extends State<QuizScreen> {
   int _currentIndex = 0;
   int _numberOfCorrects = 0;
   List<Quiz> _quizzes = [];
-  List<int> _quizIndexNumberList = [];
 
   Widget _buildRowStars() {
-    final difficulty = _quizzes.isEmpty
-        ? 0
-        : _quizzes[_quizIndexNumberList[_currentIndex]].difficulty;
+    final difficulty = _quizzes.isEmpty ? 0 : _quizzes[_currentIndex].difficulty;
     final boolList = List<bool>.generate(5, (index) => index < difficulty);
     return Row(
       children: boolList.map((isFilled) {
@@ -32,25 +29,18 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 
   Widget _buildColumnButtons() {
-    List<String> choices = _quizzes.isEmpty
-        ? []
-        : _quizzes[_quizIndexNumberList[_currentIndex]].choices;
+    List<String> choices = _quizzes.isEmpty ? [] : _quizzes[_currentIndex].choices;
     choices.shuffle();
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: choices.map((choice) {
         return OutlinedButton(
           onPressed: () {
-            final answer = _quizzes.isEmpty
-                ? ''
-                : _quizzes[_quizIndexNumberList[_currentIndex]].answer;
+            final answer = _quizzes.isEmpty ? '' : _quizzes[_currentIndex].answer;
 
             setState(() {
-              _numberOfCorrects =
-                  answer == choice ? _numberOfCorrects + 1 : _numberOfCorrects;
-              _currentIndex = _currentIndex < _quizzes.length
-                  ? _currentIndex + 1
-                  : _currentIndex;
+              _numberOfCorrects = answer == choice ? _numberOfCorrects + 1 : _numberOfCorrects;
+              _currentIndex = _currentIndex < _quizzes.length ? _currentIndex + 1 : _currentIndex;
             });
 
             if (_currentIndex == _quizzes.length) {
@@ -72,14 +62,11 @@ class _QuizScreenState extends State<QuizScreen> {
 
     Json.load(JsonFile.Quiz).then((jsonString) {
       final json = jsonDecode(jsonString);
-      final List<dynamic> quizzesJson = json['quizzes'];
+      List<dynamic> quizzesJson = json['quizzes'];
+      quizzesJson.shuffle();
 
       setState(() {
         _quizzes = quizzesJson.map((e) => Quiz.fromJson(e)).toList();
-        _quizIndexNumberList = _quizzes.isEmpty
-            ? [0]
-            : List.generate(_quizzes.length, (index) => index);
-        _quizIndexNumberList.shuffle();
       });
     });
   }
@@ -98,10 +85,10 @@ class _QuizScreenState extends State<QuizScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           Text(
-            '第${_currentIndex + 1}問 ${_quizzes.isEmpty ? '' : _quizzes[_quizIndexNumberList[_currentIndex]].genre}',
+            '第${_currentIndex + 1}問 ${_quizzes.isEmpty ? '' : _quizzes[_currentIndex].genre}',
             style: TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.bold,
@@ -112,9 +99,7 @@ class _QuizScreenState extends State<QuizScreen> {
             height: 12,
           ),
           Text(
-            _quizzes.isEmpty
-                ? '読み込み中'
-                : _quizzes[_quizIndexNumberList[_currentIndex]].question,
+            _quizzes.isEmpty ? '読み込み中' : _quizzes[_currentIndex].question,
             style: TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.bold,
