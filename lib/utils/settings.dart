accd6bf7e8064709cba0456b6ea1705547006a60
hayabusabusa <hayabusabusaSY@gmail.com>
2021-05-03 01:33:36 +0900

    refactor: fix settings functions and add document comment

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,3 +1,4 @@
+/// 設定として管理する項目を表すキーの一覧
 enum SettingsKey {
   /// クイズの問題数
   NumberOfQuiz
@@ -35,14 +36,29 @@ class Settings {
     }
   }
 
-  void setValue({
+  /// 指定したキーに対応する値をセットする.
+  void setValue<T>({
     required SettingsKey key, 
-    required dynamic value
+    required T value
   }) {
     final keyString = _mapKeyToString(key);
+
+    // NOTE: 初回の場合は Map に追加して終了.
+    if (!_keyValue.containsKey(keyString)) {
+      _keyValue.addEntries([MapEntry(keyString, value)]);
+      return;
+    }
+
+    // NOTE: 念のため型のチェックをして、違うなら何もしない.
+    final storedValue = _keyValue[keyString];
+    if (!storedValue is T) {
+      return;
+    }
+
     _keyValue.update(keyString, (_) => value);
   }
-
+ 
+ /// 指定したキーに対応する値を取り出す.
   T? getValue<T>({
     required SettingsKey key,
   }) {
@@ -57,6 +73,7 @@ class Settings {
       : null;
   }
 
+  /// 指定したキーと値を削除する.
   void remove({
     required SettingsKey key
   }) {
