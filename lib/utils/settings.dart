4b717b3730cd3cefca5d54135753ced25d05d200
hayabusabusa <hayabusabusaSY@gmail.com>
2021-05-03 01:14:11 +0900

    feature: impl settings functions

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,3 +1,8 @@
+enum SettingsKey {
+  /// クイズの問題数
+  NumberOfQuiz
+}
+
 /// アプリに関する設定を管理するシングルトンクラス.
 class Settings {
 
@@ -16,7 +21,39 @@ class Settings {
     return newInstance;
   }
 
+  Map<String, dynamic> _keyValue = {};
+
   // NOTE: 名前付きコンストラクタをプライベートにして、コンストラクタをプライベートでのみ利用できるようにしている.
   // こうすることで上記の `instance` からのみインスタンスを取得するように強制することができる.
   Settings._();
+
+  /// `Enum` から `String` に変換するメソッド.
+  String _mapKeyToString(SettingsKey key) {
+    switch (key) {
+      case SettingsKey.NumberOfQuiz:
+        return 'number_of_quiz';
+    }
+  }
+
+  void setValue({
+    required SettingsKey key, 
+    required dynamic value
+  }) {
+    final keyString = _mapKeyToString(key);
+    _keyValue.update(keyString, (_) => value);
+  }
+
+  T? getValue<T>({
+    required SettingsKey key,
+  }) {
+    final keyString = _mapKeyToString(key);
+    if (!_keyValue.containsKey(keyString)) {
+      return null;
+    }
+
+    final value = _keyValue[keyString];
+    return value is T 
+      ? value 
+      : null;
+  }
 }
